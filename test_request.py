c6035b27df49554e833279920bae2b1c2cfcbe00
Arnau2002 <arnaucliment@gmail.com>
2025-10-23 12:48:42 +0200

    comentar cada paso de test

--- test_request.py
+++ test_request.py
@@ -13,6 +13,7 @@ ALQUILER_ID = 1 # ID esperado para el primer alquiler
 time.sleep(2) # Pequeña espera por si la DB tarda en iniciar
 
 
+#DESCOMENTAR PASO A PASO PARA PROBAR CADA SECCIÓN!!!
 
 
 #####################################
@@ -46,34 +47,36 @@ print(f"Código: {response.status_code} | Total de películas: {len(response.jso
 ##################################################
 # 2. FLUJO TRANSACCIONAL: ALQUILER Y DEVOLUCIÓN
 ###################################################
-print("\n"+"="*34)
-print("2. ALQUILER Y DEVOLUCIÓN (TRANSACCIÓN)")
-print("="*34)
 
-# 2.1 POST Alquiler (ÉXITO)
-print("\n--> POST /alquileres/ (Alquilar)")
-response = requests.post(f"{BASE_URL}/alquileres/", json=ALQUILER_DATA)
-print(f"Código: {response.status_code} | Alquiler ID: {response.json().get('id', 'ERROR')}")
 
-# 2.2 GET Verificación de Stock (DEBE ESTAR NO DISPONIBLE --> Flase)
-print("--> GET /peliculas/ (Verificar Stock)")
-response = requests.get(f"{BASE_URL}/peliculas/")
-print(f"Código: {response.status_code} | Película 1 Disponible: {response.json()[0]['disponible']}")
+# print("\n"+"="*34)
+# print("2. ALQUILER Y DEVOLUCIÓN (TRANSACCIÓN)")
+# print("="*34)
 
-# 2.3 POST Alquiler fallido (Prueba de stock. Debe fallar con 400)
-print("\n--> POST /alquileres/ (Fallo por Stock)")
-response = requests.post(f"{BASE_URL}/alquileres/", json=ALQUILER_DATA)
-print(f"Código: {response.status_code} | Detalle de error: {response.json().get('detail', 'OK')}")
+# # 2.1 POST Alquiler (ÉXITO)
+# print("\n--> POST /alquileres/ (Alquilar)")
+# response = requests.post(f"{BASE_URL}/alquileres/", json=ALQUILER_DATA)
+# print(f"Código: {response.status_code} | Alquiler ID: {response.json().get('id', 'ERROR')}")
 
-# 2.4 DELETE Devolución (Usa el ID 1)
-print("\n--> DELETE /alquileres/{id} (Devolución Formal)")
-response = requests.delete(f"{BASE_URL}/alquileres/{ALQUILER_ID}")
-print(f"Código: {response.status_code} | Estado: Devolución completada (204 esperado)")
+# # 2.2 GET Verificación de Stock (DEBE ESTAR NO DISPONIBLE --> Flase)
+# print("--> GET /peliculas/ (Verificar Stock)")
+# response = requests.get(f"{BASE_URL}/peliculas/")
+# print(f"Código: {response.status_code} | Película 1 Disponible: {response.json()[0]['disponible']}")
 
-# 2.5 GET Verificación de Stock (DEBE ESTAR DISPONIBLE DE NUEVO --> True)
-print("--> GET /peliculas/ (Verificar Stock después)")
-response = requests.get(f"{BASE_URL}/peliculas/")
-print(f"Código: {response.status_code} | Película 1 Disponible: {response.json()[0]['disponible']}")
+# # 2.3 POST Alquiler fallido (Prueba de stock. Debe fallar con 400)
+# print("\n--> POST /alquileres/ (Fallo por Stock)")
+# response = requests.post(f"{BASE_URL}/alquileres/", json=ALQUILER_DATA)
+# print(f"Código: {response.status_code} | Detalle de error: {response.json().get('detail', 'OK')}")
+
+# # 2.4 DELETE Devolución (Usa el ID 1)
+# print("\n--> DELETE /alquileres/{id} (Devolución Formal)")
+# response = requests.delete(f"{BASE_URL}/alquileres/{ALQUILER_ID}")
+# print(f"Código: {response.status_code} | Estado: Devolución completada (204 esperado)")
+
+# # 2.5 GET Verificación de Stock (DEBE ESTAR DISPONIBLE DE NUEVO --> True)
+# print("--> GET /peliculas/ (Verificar Stock después)")
+# response = requests.get(f"{BASE_URL}/peliculas/")
+# print(f"Código: {response.status_code} | Película 1 Disponible: {response.json()[0]['disponible']}")
 
 
 
